c9f65b7e4916f5adc8357fbe492e96413580962d
sirkaue <kauekdslopes@gmail.com>
2023-02-23 22:03:19 -0300

    commit

---
+++ app/src/main/java/br/com/etecia/appcar/Janela_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.appcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Janela_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.janela_layout);
+    }
+}
